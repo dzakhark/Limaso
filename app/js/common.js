d1533ade3dbd5a2885dd8b515f8e421e8dd21fee
Dmitriy Zakharchuk <dmitry.zakh1@gmail.com>
2018-02-06 14:49:39 +0300

    Added accordeon

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,10 @@
 $(function() {
 
-	// Custom JS
+	$('.accordeon .acc-head').on('click', funcAcc);
 
+	function funcAcc() {
+        $('.accordeon .acc-text').not($(this).next()).slideUp(500);
+        $('.accordeon .acc-head').not($(this)).removeClass('active');
+        $(this).toggleClass('active').next().slideToggle(500);
+    }
 });

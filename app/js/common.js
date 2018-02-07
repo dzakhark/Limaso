ec851b76d4360147099eed4f1e8d5be24db15a3e
Dmitriy Zakharchuk <dmitry.zakh1@gmail.com>
2018-02-07 22:17:03 +0300

    Added range-price, sort-dropdown and product list

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,69 @@
 $(function() {
 
-	$('.accordeon .acc-head').on('click', funcAcc);
+    var priceFromElem = $('.price-from');
+    var priceToElem = $('.price-to');
+    priceFromElem.val(0);
+    priceToElem.val(1400);
 
-	function funcAcc() {
+    var priceFrom = priceFromElem.val().length ? $('.price-from').val() : 0;
+    var priceTo = priceToElem.val().length ? $('.price-to').val() : 1400;
+
+    $('.range-form').on('change', function (e) {
+        priceFrom = priceFromElem.val();
+        priceTo = priceToElem.val();
+        if (priceFrom > priceTo) {
+            priceTo = priceFrom;
+            priceToElem.val(priceFrom);
+        }
+
+        $("#slider-range").slider({
+            range: true,
+            min: 0,
+            max: 1400,
+            values: [priceFrom, priceTo],
+            slide: function (event, ui) {
+                priceFromElem.val(ui.values[0]);
+                priceToElem.val(ui.values[1]);
+            }
+        });
+    });
+
+    $('.accordeon .acc-head').on('click', funcAcc);
+
+    function funcAcc() {
         $('.accordeon .acc-text').not($(this).next()).slideUp(500);
         $('.accordeon .acc-head').not($(this)).removeClass('active');
         $(this).toggleClass('active').next().slideToggle(500);
     }
+
+
+
+    $("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 1400,
+        values: [priceFrom, priceTo],
+        slide: function (event, ui) {
+            priceFromElem.val(ui.values[0]);
+            priceToElem.val(ui.values[1]);
+        }
+    });
+
+
+
+    $('.btn-dropdown').on('click', function () {
+        $('.dropdown-content').slideToggle(500);
+    });
+
+    $('.sort-item').on('click', function () {
+        console.log($('.btn-dropdown').text());
+        $('.btn-dropdown').text($(this).text());
+        $('.dropdown-content').slideUp(500);
+    });
+
+    $(document).click(function(event) {
+        if ($(event.target).closest('.btn-dropdown').length) return;
+        $('.dropdown-content').slideUp(500);
+        event.stopPropagation();
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,8 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.min.js', // Всегда в конце
+		'app/libs/jquery-ui/jquery-ui.js',
+		'app/js/common.min.js' // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Минимизировать весь js (на выбор)
